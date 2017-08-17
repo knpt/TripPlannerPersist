3f659ae6e444c7830eac93015827d8836bfbcccb
Mieka Page <git@coldwine.nyc>
2017-08-17 11:21:13 -0400

    lol here's the rest of the files

---
+++ routes/api/days.js
@@ -0,0 +1,32 @@
+var Promise = require('bluebird');
+var router = require('express').Router();
+var Hotel = require('../../models').Hotel;
+var Restaurant = require('../../models').Restaurant;
+var Activity = require('../../models').Activity;
+var Day = require('../../models/day.js');
+
+router.get('/days', function(req, res, next){
+  Day.findAll()
+  .then(function(days) {
+    res.send(days);
+  });
+});
+
+router.post('/days', function(req, res, next){
+  // Day.create(req.body)
+  // .then(function(day) {
+    res.send({msg: 'You created a day!!'});
+  // });
+});
+
+
+router.get('/days/:id', function(req,res, next){});
+router.delete('/days/:id', function(req,res, next){});
+router.post('/days/:id', function(req,res, next){});
+
+router.post('/days/:id/:attractions', function(req,res, next){});
+router.delete('/days/:id/:attractions', function(req,res, next){});
+
+
+
+module.exports = router;
