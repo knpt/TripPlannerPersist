26fea0099b7f31e3ab9ed9439cc69723b6843e62
Kimberly Ton <kimberlyton@gmail.com>
2017-08-17 12:04:51 -0400

    ..

--- models/day.js
+++ models/day.js
@@ -1,9 +1,20 @@
 /* eslint-disable camelcase */
 var Sequelize = require('sequelize');
 var db = require('./_db');
+var Hotel = require('./hotel');
+var Restaurant = require('./restaurant');
+var Activity = require('./activity');
 
 var Day = db.define('day', {
   name: Sequelize.INTEGER
+}, {
+  defaultScope: {
+    include: [
+      Hotel,
+      Restaurant, 
+      Activity
+    ]
+  }
 });
 
 module.exports = Day;
